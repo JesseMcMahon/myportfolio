bd471f0511574d0f63f1a476b428b964e9409c2d
Jesse McMahon <Jessemcmahon17@gmail.com>
2020-05-09 12:04:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add("current");
+  } else {
+    slides[0].classList.add("current");
+  }
+};
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add("current");
+  } else {
+    slides[slides.length - 1].classList.add("current");
+  }
+};
+
+next.addEventListener("click", () => {
+  nextSlide();
+});
+
+prev.addEventListener("click", () => {
+  prevSlide();
+});
